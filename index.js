823d13245196a697842d76f7acc04a5c01f62cd8
Umang Parmar <umangjparmar@gmail.com>
2021-05-09 21:07:56 +0530

    add logic to use upaste

--- index.js
+++ index.js
@@ -8,13 +8,9 @@ function encodeImageFileAsURL(element) {
 }
 
 function uploadPaste(uri) {
-    const data = {
-        'api_dev_key': 'qX_9wbFXNbyBcMtXFTq2CkUwzZpGlQd4',
-        'api_paste_code': uri,
-        'api_option': 'paste',
-        'api_paste_private': 2,
-        'api_folder_key': 'khusi'
-    };
-    axios.post('https://pastebin.com/api/api_post.php', data)
+    const request = new XMLHttpRequest();
+    request.open("POST", "http://upaste.herokuapp.com/", true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.send(`{paste=${uri}}`);
 }
 
