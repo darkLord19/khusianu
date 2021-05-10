cad3a76276c1cfa007374e5fd1e8f639029cb89c
Umang Parmar <umangjparmar@gmail.com>
2021-05-10 13:36:03 +0530

    update uplodaPaste logic

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ function encodeImageFileAsURL(element) {
 }
 
 function uploadPaste(uri) {
+    const body = {
+        data: uri
+    }
     const request = new XMLHttpRequest();
-    request.open("POST", "http://upaste.herokuapp.com/", true);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    request.send(`{paste=${uri}}`);
+    request.open("POST", "https://3b1twu5u9j.execute-api.ap-south-1.amazonaws.com/default/createPaste");
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(JSON.stringify(body));
 }
 
